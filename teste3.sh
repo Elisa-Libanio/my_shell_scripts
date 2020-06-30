48bc7f3b127ce80fe5391b09f10bd461fcbbfd6c
Elisa-Libanio <elisalibanio1@gmail.com>
2020-06-30 19:23:41 -0300

    Testando adicionar o arquivo teste3

--- teste3.sh
+++ teste3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-
+# Eu sou trybe e...
 VARIAVEL=`hostname`
  echo "Esse script está rodando no computador $VARIAVEL em que $VARIAVEL é o resultado do comando `hostname`que está na variavél" 
 
